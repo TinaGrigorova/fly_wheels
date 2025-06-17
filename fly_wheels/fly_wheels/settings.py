2ab883df9e70d7efd52af84c859a62c25c02850d
Tina Grigorova <tina.grigorova@students.codeinstitute.net>
2025-06-17 16:34:46 +0000

    add index.html base, models and urls

--- fly_wheels/fly_wheels/settings.py
+++ fly_wheels/fly_wheels/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = "fly_wheels.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        'DIRS': [BASE_DIR / 'store' / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -127,3 +127,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
