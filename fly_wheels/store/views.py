03527d105fee8d1e9343e604db0783bd817ea6f6
Tina Grigorova <tina.grigorova@students.codeinstitute.net>
2025-06-19 14:05:09 +0000

    add products dynamically on homepage using Product model

--- fly_wheels/store/views.py
+++ fly_wheels/store/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from products.models import Product
 
 def home(request):
-    return render(request, 'store/index.html')
+    products = Product.objects.all()
+    return render(request, 'store/index.html', {'products': products})
