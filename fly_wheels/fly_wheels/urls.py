be94c338f6a0f06841d3ce787c8937fb138733ef
Tina Grigorova <tina.grigorova@students.codeinstitute.net>
2025-06-23 15:22:13 +0000

    add accoints app and sign up view and authentication

--- fly_wheels/fly_wheels/urls.py
+++ fly_wheels/fly_wheels/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
     path('products/', include('products.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
 if settings.DEBUG:
