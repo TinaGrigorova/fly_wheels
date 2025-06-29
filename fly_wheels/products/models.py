a6728b5e6960a251571e9c40fe7ccd2e8a9cc78b
Tina Grigorova <tina.grigorova@students.codeinstitute.net>
2025-06-29 14:59:19 +0000

    add new category for different producs

--- fly_wheels/products/models.py
+++ fly_wheels/products/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 
 class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ('Alloy Wheels', 'Alloy Wheels'),
+        ('Track Edition', 'Track Edition'),
+    ]
+
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     image = models.ImageField(upload_to='product_images/')
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default='Alloy Wheels')
 
     def __str__(self):
         return self.name
