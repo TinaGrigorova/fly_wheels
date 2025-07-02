6858dabd2eb002ebc0e1c39620c3c1104d336865
Tina Grigorova <tina.grigorova@students.codeinstitute.net>
2025-07-02 15:22:45 +0000

    Fix: Display Track Edition wheels by ensuring correct category values in DB

--- fly_wheels/products/views.py
+++ fly_wheels/products/views.py
@@ -3,7 +3,7 @@ from .models import Product
 
 def shop(request):
     alloy_wheels = Product.objects.filter(category='Alloy Wheels')
-    track_edition = Product.objects.filter(category='Track Edition')
+    track_edition = Product.objects.filter(category='track')
     return render(request, 'products/shop.html', {
         'alloy_wheels': alloy_wheels,
         'track_edition': track_edition,
