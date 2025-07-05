0a006364c1228b8b99e5a0205411a57e0baa43ed
Tina Grigorova <tina.grigorova@students.codeinstitute.net>
2025-07-05 14:18:58 +0000

    Add and connect Shop category buttons (Alloy Wheels, Track Edition, View All) with working navigation

--- fly_wheels/products/urls.py
+++ fly_wheels/products/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path('', views.shop, name='shop'),
+    path('', views.shop, name='shop'),
+    path('alloy/', views.alloy_wheels, name='alloy_wheels'),  
+    path('track/', views.track_edition, name='track_edition'),
 ]
--- fly_wheels/products/views.py
+++ fly_wheels/products/views.py
@@ -8,3 +8,17 @@ def shop(request):
         'alloy_wheels': alloy_wheels,
         'track_edition': track_edition,
     })
+
+def alloy_wheels(request):
+    wheels = Product.objects.filter(category='Alloy Wheels')
+    return render(request, 'products/category.html', {
+        'title': 'Alloy Wheels',
+        'products': wheels,
+    })
+
+def track_edition(request):
+    wheels = Product.objects.filter(category='track')
+    return render(request, 'products/category.html', {
+        'title': 'Track Edition',
+        'products': wheels,
+    })
