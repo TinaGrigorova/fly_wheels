c509758c981449afda6e9ed7346c8288e640605a
Tina Grigorova <tina.grigorova@students.codeinstitute.net>
2025-07-15 13:36:35 +0000

    Add add-to-cart view, cart page, and link from product listing

--- fly_wheels/products/urls.py
+++ fly_wheels/products/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.shop, name='shop'),
     path('alloy/', views.alloy_wheels, name='alloy_wheels'),  
     path('track/', views.track_edition, name='track_edition'),
+    path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('cart/', views.view_cart, name='view_cart'),
 ]
--- fly_wheels/products/views.py
+++ fly_wheels/products/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
 from .models import Product
+from django.shortcuts import get_object_or_404, redirect
+from .models import Product, Order, CartItem
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def add_to_cart(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    order, created = Order.objects.get_or_create(user=request.user, is_paid=False)
+    cart_item, created = CartItem.objects.get_or_create(order=order, product=product)
+
+    if not created:
+        cart_item.quantity += 1
+        cart_item.save()
+
+    return redirect('view_cart')
+
+@login_required
+def view_cart(request):
+    order = Order.objects.filter(user=request.user, is_paid=False).first()
+    return render(request, 'products/cart.html', {'order': order})
+
 
 def shop(request):
     alloy_wheels = Product.objects.filter(category='Alloy Wheels')
@@ -22,3 +43,4 @@ def track_edition(request):
         'title': 'Track Edition',
         'products': wheels,
     })
+
