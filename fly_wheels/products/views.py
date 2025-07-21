197f2999f660b1ccdc81e9e426173bbf9695b0c9
Tina Grigorova <tina.grigorova@students.codeinstitute.net>
2025-07-21 13:35:20 +0000

    update views.py to use model-based cart with Order and CartItem

--- fly_wheels/products/views.py
+++ fly_wheels/products/views.py
@@ -1,50 +1,9 @@
 from django.contrib import messages
-from django.shortcuts import get_object_or_404, redirect
-from .models import Product, Order, CartItem
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404
+from .models import Product, Order, CartItem
 
-def add_to_cart(request, product_id):
-    product = get_object_or_404(Product, id=product_id)
-    quantity = int(request.POST.get('quantity', 1))
-
-    cart = request.session.get('cart', {})
-
-    if str(product_id) in cart:
-        cart[str(product_id)] += quantity
-    else:
-        cart[str(product_id)] = quantity
-
-    request.session['cart'] = cart
-    messages.success(request, f"Added {product.name} to your cart!")
-    return redirect('view_cart') #redirection
-
-def view_cart(request):
-    order = get_object_or_404(Order, user=request.user, is_ordered=False)
-    total = sum(item.subtotal for item in order.items.all())
-    return render(request, 'products/cart.html', {
-        'order': order,
-        'total': total,
-    })
-
-@login_required
-def add_to_cart(request, product_id):
-    product = get_object_or_404(Product, id=product_id)
-    order, created = Order.objects.get_or_create(user=request.user, is_paid=False)
-    cart_item, created = CartItem.objects.get_or_create(order=order, product=product)
-
-    if not created:
-        cart_item.quantity += 1
-        cart_item.save()
-
-    return redirect('view_cart')
-
-@login_required
-def view_cart(request):
-    order = Order.objects.filter(user=request.user, is_paid=False).first()
-    return render(request, 'products/cart.html', {'order': order})
-
-
+# Shop Views
 def shop(request):
     alloy_wheels = Product.objects.filter(category='Alloy Wheels')
     track_edition = Product.objects.filter(category='track')
@@ -67,3 +26,21 @@ def track_edition(request):
         'products': wheels,
     })
 
+# Cart Views 
+@login_required
+def add_to_cart(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    order, created = Order.objects.get_or_create(user=request.user, is_paid=False)
+    cart_item, created = CartItem.objects.get_or_create(order=order, product=product)
+
+    if not created:
+        cart_item.quantity += 1
+        cart_item.save()
+
+    messages.success(request, f"Added {product.name} to your cart!")
+    return redirect('view_cart')
+
+@login_required
+def view_cart(request):
+    order = Order.objects.filter(user=request.user, is_paid=False).first()
+    return render(request, 'products/cart.html', {'order': order})
