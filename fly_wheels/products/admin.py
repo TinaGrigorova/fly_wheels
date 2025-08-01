67ccdb6ef3fb239efea0323b0efee37b1c4915ab
Tina Grigorova <tina.grigorova@students.codeinstitute.net>
2025-08-01 11:10:18 +0000

    Update admin to include brand and size filters for products

--- fly_wheels/products/admin.py
+++ fly_wheels/products/admin.py
@@ -2,3 +2,9 @@ from django.contrib import admin
 from .models import Product
 
 admin.site.register(Product)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'brand', 'size', 'price')
+    list_filter = ('category', 'brand', 'size')
+    search_fields = ('name',)
